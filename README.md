c8dd5c96564ff9d1875f31400ca9d8c411e4866c
Ihab <Ihab@users-MacBook-Pro.local>
2017-04-03 00:56:16 -0400

    updating read me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Train-Time
+
+==================
+
+## This app will calulate the arrival time according to the first arrival and interval beteen each train
+
+Thank you for looking at my code
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -36,7 +36,7 @@ $("#addTrain").on("click", function(event) {
 
     $("#name").val("");
     $("#destination").val("");
-    $("#time").val("");
+    $("#firstTrain").val("");
     $("#frequency").val("");
 
 
@@ -50,7 +50,7 @@ database.ref().on("child_added", function(childSnapshot) {
         var frequency = childSnapshot.val().frequency;
         var firstTrain = childSnapshot.val().firstTrain;
         var remainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
-        var minutes = frequency - remainder;
+        var minutes = (frequency - remainder);
         var arrival = moment().add(minutes, "m").format("hh:mm A");
         console.log(remainder);
         console.log(minutes);
@@ -70,65 +70,6 @@ database.ref().on("child_added", function(childSnapshot) {
     function(errorObject) {
 
     });
-
-/*
-var name = $("#name").val().trim();
-var destination = $("#destination").val().trim();
-var time = moment($("#time").val().trim(), "HH:mm").subtract(10, "years").format("x");
-var frequency = $("#frequency").val().trim();
-var remainder = moment().diff(moment.unix(time), "minutes") % frequency;
-var minutesAway = frequency - remainder
-var arrival = moment().add(minutes, "m").format("hh:mm A");
-
-
-// 2. Button for adding a Train
-$("#submitBtn").on("click", function(event) {
-    event.preventDefault();
-    console.log("hello");
-    // Grabbed values from text boxes
-    name = $("#name").val().trim();
-    destination = $("#destination").val().trim();
-    time = $("#time").val().trim();
-    frequency = $("#frequency").val().trim();
-    minutesAway = ((currentTime - time) / 60).toString();
-    console.log(minutesAway);
-
-    // Code for handling the push
-    database.ref().push({
-        name: name,
-        destination: destination,
-        time: time,
-        frequency: frequency,
-        minutesAway: minutesAway,
-
-    });
-    alert("New Train successfully added");
-    $("#name").val("");
-    $("#destination").val("");
-    $("#time").val("");
-    $("#frequency").val("");
-
-
-
-});
-
-// 3. Create Firebase event for adding a train to the database and a row in the html when a user adds an entry
-database.ref().on("child_added", function(childSnapshot) {
-
-        $("tbody").append("<tr>" +
-                "<td>" + childSnapshot.val().name + "</td>" +
-                "<td>" + childSnapshot.val().destination + "</td>" +
-                "<td>" + childSnapshot.val().frequency + "</td>" +
-                "<td>" + childSnapshot.val().arrival + "</td>" +
-
-                "<td>" + childSnapshot.val().minutesAway) + "</td>" +
-
-            "</tr>+";
-
-    },
-    function(errorObject) {
-
-
-
-    });
-    */
+setTimeout(function() {
+    window.location.reload(1);
+}, 60 * 1000);
